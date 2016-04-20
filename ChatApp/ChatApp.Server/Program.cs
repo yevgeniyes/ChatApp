e4ce98648d5b24d1adfab7518d798013d506e496
Евгений Есауленко <esaulenko.lenovo@gmail.com>
2016-04-20 23:53:06 +0300

    first step

---
+++ ChatApp/ChatApp.Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = new TokenGenerator();
+
+            Random random = new Random();
+
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine(token.GetToken(random));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ChatApp/ChatApp.Server/TokenGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Server
+{
+    class TokenGenerator
+    {
+        public string GetToken(Random random)
+        {
+            string[] simbols = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "q", "w", "e", "r", "t", "y",
+            "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"};
+
+            string token = "token_";
+            int tokenLength = 30;
+
+            for (int i = 0; i <= tokenLength; i++)
+            {
+                token += simbols[random.Next(simbols.Length)];
+            }
+            return token;
+        }
+    }
+}
